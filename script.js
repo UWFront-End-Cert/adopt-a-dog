58ec6bea9f6c1c2f8c7929395a30b8464ba8f05a
Jay Chakravarty <jaychakravarty@icloud.com>
2024-06-07 15:04:22 -0700

    button click handlers to increment and alert total

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function information(name, breed, adoptionFees) {
     alert(`Name: ${name}\nBreed: ${breed}\nFees: $${adoptionFees}`);
 }
+let total = 0;
+function adopt() {
+    total += 123.45
+    alert(`Total: $${total.toFixed(2)}`);
+}
