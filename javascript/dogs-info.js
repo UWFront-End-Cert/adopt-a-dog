b084485f173ff7e7b84c34b99b7657fbc0151445
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-06-07 16:00:08 -0700

    added click handlers that alerts the dog’s information when clicking on the dog’s photo to index.html

---
+++ javascript/dogs-info.js
@@ -0,0 +1,39 @@
+function murphyInfo() {
+    let information = 'Murphy is a pitbull boxer mix and the adoption fee is $50';
+    alert(information);
+}
+
+function poppyInfo() {
+    let information = 'Poppy is a maltese mix and the adoption fee is $50';
+    alert(information);
+}
+
+function jackInfo() {
+    let information = 'Jack is a beagle and the adoption fee is $50';
+    alert(information);
+}
+
+function duffyInfo() {
+    let information = 'Duffy is a pitbull and the adoption fee is $50';
+    alert(information);
+}
+
+function lucasInfo() {
+    let information = 'Lucas is a German Shepherd mix and the adoption fee is $50';
+    alert(information);
+}
+
+function jakeInfo() {
+    let information = 'Jake is a yellow lab and the adoption fee is $50';
+    alert(information);
+}
+
+function angusInfo() {
+    let information = 'Angus is a bull dog terrier mix and the adoption fee is $50';
+    alert(information);
+}
+
+function violetInfo() {
+    let information = 'Violet is a chocolate lab and the adoption fee is $50';
+    alert(information);
+}
