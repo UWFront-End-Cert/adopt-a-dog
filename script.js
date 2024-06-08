3015dba59cd5fcb5574f81bf447651b30aa42715
Jay Chakravarty <jaychakravarty@icloud.com>
2024-06-08 15:47:24 -0700

    adding dog name to alert message

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function information(name, breed, adoptionFees) {
     alert(`Name: ${name}\nBreed: ${breed}\nFees: $${adoptionFees}`);
 }
 let total = 0;
-function adopt() {
-    total += 123.45
-    alert(`Total: $${total.toFixed(2)}`);
+function adopt(name) {
+    total += 123.45;
+    alert(`You added ${name} to your cart for adoption!\nTotal: $${total.toFixed(2)}`);
 }
