0dd837522f290e4306432e636d5e429137e6d55f
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-06-22 11:46:21 -0700

    finished code to generate all blog post dynamically with javascript

--- javascript/blog.js
+++ javascript/blog.js
@@ -1,7 +1,28 @@
 const blogPosts = [
     {
         image: 'images/blog-1.jpg',
+        imageAlt: 'woman with her arm around her black dog with their backs to the camera sitting on a rock looking out over a canyon landscape',
+        imageWidth: '328',
+        imageHeight: '310',
         title: 'Traveling With Your Dog',
+        text1: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
+        text2: 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, conet labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea adqui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alitulparume nonseca estorer spernam.'
+    },
+    {
+        image: 'images/blog-2.jpg',
+        imageAlt: 'a human hand is holding a leash that is connected to 4 dogs standing in a row on a brick floor patio',
+        imageWidth: '328',
+        imageHeight: '310',
+        title: 'How To Walk Multiple Dogs',
+        text1: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
+        text2: 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, conet labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea adqui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alitulparume nonseca estorer spernam.'
+    },
+    {
+        image: 'images/blog-3.jpg',
+        imageAlt: 'silhouttes of a girl holding up a ball to a patiently waiting dog up against a background of a sunset',
+        imageWidth: '328',
+        imageHeight: '310',
+        title: 'Teach Your Dog To Fetch!',
         text1: 'Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.',
         text2: 'Uptiusd andesci qui nem aut vendion ectur? Debis que explaut laborenia que doluptur, conet labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea adqui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alitulparume nonseca estorer spernam.'
     }
@@ -13,6 +34,9 @@ for (let i = 0; i < blogPosts.length; i += 1) {
 
     const blogImage = document.createElement('img');
     blogImage.src = blogPost.image;
+    blogImage.alt = blogPost.imageAlt;
+    blogImage.width = blogPost.imageWidth;
+    blogImage.height = blogPost.imageHeight;
 
     const blogTitle = document.createElement('h2');
     blogTitle.textContent = blogPost.title;
