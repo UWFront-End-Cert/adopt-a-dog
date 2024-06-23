9faa21f52433a63f2f62ad1f4cbf09257030260b
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-06-23 16:33:49 -0700

    created more elegant code to console.log the form inputs in contact.html

--- javascript/contact-form.js
+++ javascript/contact-form.js
@@ -1,14 +1,10 @@
 addEventListener('submit', function(event) {
     event.preventDefault();
     this.alert('Thank you. The form information has been received.');
-});
 
-
-function formInfo() {
     let cFInputs = document.querySelectorAll('input.cFInput, select.cFInput, textarea.cFInput');
-    
     for (let i = 0; i < cFInputs.length; i += 1) {
         const cFInput = cFInputs[i];
         console.log(cFInput.value);
     }
-}
+});
