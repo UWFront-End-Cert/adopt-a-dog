0235373be2129e67669a0ea08e560f8ebedce073
jordanmollot <159092302+jordanmollot@users.noreply.github.com>
2024-06-23 16:03:17 -0700

    created more elegant code to consol.log the form inputs in contact.html

--- javascript/contact-form.js
+++ javascript/contact-form.js
@@ -5,41 +5,10 @@ addEventListener('submit', function(event) {
 
 
 function formInfo() {
-    //const firstName = document.getElementById('firstName');
-    //const firstNameValue = firstName.value;
-    //console.log(firstNameValue);
-
-    //const lastName = document.getElementById('lastName');
-    //const lastNameValue = lastName.value;
-    //console.log(lastNameValue);
-
-    //const email = document.getElementById('email');
-    //const emailValue = email.value;
-    //console.log(emailValue);
-
-    //const phone = document.getElementById('phone');
-    //const phoneValue = phone.value;
-    //console.log(phoneValue);
-
-    //const contactMethod = document.getElementById('method');
-    //const contactMethodValue = contactMethod.value;
-    //console.log(contactMethodValue);
-
-    const form = document.getElementById('contactForm')
-    const formValues = new FormData(form);
-    const firstName = formValues.get('applicantFirstName');
-    const lastName = formValues.get('applicantLastName');
-    const email = formValues.get('applicantEmail');
-    const phone = formValues.get('applicantPhone');
-    const method = formValues.get('method');
-    const howHear = formValues.get('howHear');
-    const helpQuestion = formValues.get('helpQuestion');
-
-    console.log(firstName);
-    console.log(lastName);
-    console.log(email);
-    console.log(phone);
-    console.log(method);
-    console.log(howHear);
-    console.log(helpQuestion);
+    let cFInputs = document.querySelectorAll('input.cFInput, select.cFInput, textarea.cFInput');
+    
+    for (let i = 0; i < cFInputs.length; i += 1) {
+        const cFInput = cFInputs[i];
+        console.log(cFInput.value);
+    }
 }
